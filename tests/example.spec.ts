4a1d63b78ac2d48830e45aee51a6acc1b1023f5a
Alvaro <alvaro78an@gmail.com>
2023-10-09 09:50:25 +0200

    Continue with example code test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,12 +7,40 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('get started link', async ({ page }) => {
+		await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+		// Click the get started link.
+		await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+		// Expects page to have a heading with the name of Installation.
+		await expect(
+			page.getByRole('heading', { name: 'Installation' })
+		).toBeVisible();
+	});
+
+
+  // Nos permite interactuar con la página web
+test('visit main page', async ({ page }) => {
+	// Navega
+	await page.goto('http://mercadolibre.com.co');
+	// Busca el elemento por el id y lo llena
+	await page.locator("input[id='cb1-edit']").fill('iphone');
+	// Se presiona el boton enter
+	await page.keyboard.press('Enter');
+
+	await expect(
+		page.locator("//ol[contains(@class, 'ui-search-layout')]")
+	).toBeVisible();
+	// await page.pause();
+
+	const titles = await page
+		.locator("//ol[contains(@class, 'ui-search-layout')]//li//h2")
+		.allInnerTexts();
+
+	for (let title of titles) {
+		console.log('the title is: ', title);
+	}
 });
+
+
